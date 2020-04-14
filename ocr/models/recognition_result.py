633ae6924212f3d82e96bdf0eaef17763b099e06
Btapple <bt_apple@qq.com>
2020-04-14 19:57:58 +0800

    init ocr app (#3)

---
+++ ocr/models/recognition_result.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.postgres.fields import JSONField
+from ocr.models.project import Project
+
+class RecognitionResult(models.Model):
+    belong_to = models.ForeignKey(to=Project, on_delete=models.CASCADE)
+    result = JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
