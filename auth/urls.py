db1240d4a96864c988fe1766efe696e8275944e2
yankuai <bhsfermy@gmail.com>
2020-04-18 00:41:33 +0800

    rename auth

--- auth/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from auth.api.auth import login
-from auth.api.user_management import create_user
-urlpatterns = [
-    path("", login),
-    path("create", create_user)
-]
