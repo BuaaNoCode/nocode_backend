8f4fb263b255439332f207e8e4b1bd0c9490b166
yankuai <bhsfermy@gmail.com>
2020-04-29 16:32:17 +0800

    ocr handler

--- ocr/api/ocr_result.py
+++ ocr/api/ocr_result.py
@@ -11,6 +11,7 @@ from ocr.models.ocr_api_record import OCRApiRecord
 from ocr.models.project import Project
 from ocr.models.recognition_result import RecognitionResult
 from user_manager.interface import auth_required
+from ocr.ocrtool.form_recognizer_layout import form_recognizer_layout
 
 
 class ApiIndex(Enum):
@@ -35,10 +36,32 @@ def receieve_ocr_photo(request: HttpRequest, project_id: int):
         return failed_api_response(StatusCode.REFUSE_ACCESS)
     
     # TODO:: fetch something from request and pass them to ocr_handler
+    json_text = request.FILES.get("json", None)
+    load = json.load(json_text)
+    rname = load["name"]
+    rcomment = load["comment"]
+    img_file = request.FILES.get("file", None)
+
+    result_json = ocr_handler(img_file)
+    result: RecognitionResult = RecognitionResult(
+        name = rname,
+        comment = rcomment,
+        belong_to = project,
+        result = result_json
+    )
+    result.save()
+
+    res_data = {
+        "id": result.id,
+        "created_at": result.created_at
+    }
+    return success_api_response(res_data)
 
 
-def ocr_handler():
-    pass
+def ocr_handler(img_file):
+    source = ""
+    r_json = form_recognizer_layout(source, img_file, img_file.content_type)
+    return r_json
 
 
 @response_wrapper
