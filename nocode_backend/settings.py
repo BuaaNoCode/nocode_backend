9a9254a4e4376759c9624c9b224ab2ed8101b2e4
btapple <bt_apple@qq.com>
2020-05-03 22:58:49 +0800

    fix typo

--- nocode_backend/settings.py
+++ nocode_backend/settings.py
@@ -99,9 +99,9 @@ if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'nocode',
-            'USER': 'nocode',
-            'PASSWORD': 'nocode',
+            'NAME': 'github_actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
             'HOST': '127.0.0.1',
             'PORT': '5432',
         }
