bd850b7bfee889820c70fc3be0dcf28a3fe3bb5b
btapple <bt_apple@qq.com>
2020-05-24 00:05:25 +0800

    fix mail backend

--- nocode_backend/settings.py
+++ nocode_backend/settings.py
@@ -119,7 +119,7 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
