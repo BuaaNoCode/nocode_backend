bd1eb1824e851a118de4e09c9921af04db59ad12
btapple <bt_apple@qq.com>
2020-05-28 17:17:02 +0800

    add tencent ocr and baidu ocr

--- ocr/ocrtool/azure.py
+++ ocr/ocrtool/azure.py
@@ -1,41 +1,43 @@
 import json
 import time
 
+from django.conf import settings
 from requests import get, post
+
+_azure_endpoint = r"{}".format(settings.AZURE_ENDPOINT)
+_azure_apim_key = settings.AZURE_APIM_KEY
 
 
 def azure_form_recognizer_layout(files, content_type):
-    endpoint = r" https://nocode-form-2.cognitiveservices.azure.com"
-    apim_key = "d7596252a6f94d778db5897122013465"
-    post_url = endpoint + "/formrecognizer/v2.0-preview/Layout/analyze"
+    post_url = _azure_endpoint + "/formrecognizer/v2.0-preview/Layout/analyze"
     headers = {
         'Content-Type': content_type,
-        'Ocp-Apim-Subscription-Key': apim_key,
+        'Ocp-Apim-Subscription-Key': _azure_apim_key,
     }
     data_bytes = files.read()
     try:
         resp = post(url=post_url, data=data_bytes, headers=headers)
         if resp.status_code != 202:
-            return json.loads(resp.text)
+            return False, json.loads(resp.text)
         get_url = resp.headers["operation-location"]
     except Exception as e:
-        return json.loads(('{"error":{"code":"POSTUnkownError", "message":"'+str(e)+'"}}'))
+        return False, json.loads(('{"error":{"code":"POSTUnkownError", "message":"'+str(e)+'"}}'))
     n_tries = 10
     n_try = 0
     wait_sec = 6
     while n_try < n_tries:
         try:
-            resp = get(url=get_url, headers={"Ocp-Apim-Subscription-Key": apim_key})
+            resp = get(url=get_url, headers={"Ocp-Apim-Subscription-Key": _azure_apim_key})
             resp_json = json.loads(resp.text)
             if resp.status_code != 200:
-                return resp_json
+                return False, resp_json
             status = resp_json["status"]
             if status == "succeeded":
-                return resp_json
+                return True, resp_json
             if status == "failed":
-                return json.loads('{"error":{"code":"AnalysisFailed", "message":"Can\'t analyze"}}')
+                return False, json.loads('{"error":{"code":"AnalysisFailed", "message":"Can\'t analyze"}}')
             time.sleep(wait_sec)
             n_try += 1
         except Exception as e:
-            return json.loads(('{"error":{"code":"GETUnkownError", "message":"' + str(e) + '"}}'))
-    return json.loads(('{"error":{"code":"GETTimeLimitExceeded", "message":"may try later"}}'))
+            return False, json.loads(('{"error":{"code":"GETUnkownError", "message":"' + str(e) + '"}}'))
+    return False, json.loads(('{"error":{"code":"GETTimeLimitExceeded", "message":"may try later"}}'))
---
+++ ocr/ocrtool/tencent.py
@@ -0,0 +1,96 @@
+import csv
+import json
+
+from django.conf import settings
+from qcloud_cos import CosConfig, CosS3Client
+from tencentcloud.common import credential
+from tencentcloud.common.exception.tencent_cloud_sdk_exception import \
+    TencentCloudSDKException
+from tencentcloud.common.profile.client_profile import ClientProfile
+from tencentcloud.common.profile.http_profile import HttpProfile
+from tencentcloud.ocr.v20181119 import models, ocr_client
+
+_tencent_bucket_url = r"{}".format(settings.TENCENT_BUCKET_URL)
+_tencent_bucket_name = settings.TENCENT_BUCKET_NAME
+_tencent_bucket_secret_id = settings.TENCENT_BUCKET_SECRET_ID
+_tencent_bucket_secret_key = settings.TENCENT_BUCKET_SECRET_KEY
+_tencent_credential_1 = settings.TENCENT_CREDENTIAL_1
+_tencent_credential_2 = settings.TENCENT_CREDENTIAL_2
+
+
+_tencent_bucket_secret_id = 'AKIDpUV2IH8SespWx7CF6i3LMOAft190bwFT'
+_tencent_bucket_secret_key = '6wV5PUaTBftdBUXSBi02sWv47uTnqhNn'
+_region = 'ap-beijing'
+_token = None
+_scheme = 'https'
+_config = CosConfig(Region=_region, SecretId=_tencent_bucket_secret_id,
+                    SecretKey=_tencent_bucket_secret_key, Token=_token, Scheme=_scheme)
+
+
+def upload_bucket(file):
+    # 获取客户端对象
+    client = CosS3Client(_config)
+    # 存储在腾讯云的文件名
+    picName = r"nocode_pic.jpg"
+    # 本地的图片路径
+    response = client.put_object(
+        Bucket=_tencent_bucket_name,
+        Body=file,
+        Key=picName,
+        StorageClass='STANDARD',
+        EnableMD5=False,
+        ACL='public-read'
+    )
+    # 图片在腾讯云的url
+    return _tencent_bucket_url+picName
+
+
+def tencent_ocr_api(url):
+    try:
+        cred = credential.Credential(_tencent_credential_1, _tencent_credential_2)
+        httpProfile = HttpProfile()
+        httpProfile.endpoint = "ocr.tencentcloudapi.com"
+
+        clientProfile = ClientProfile()
+        clientProfile.httpProfile = httpProfile
+        client = ocr_client.OcrClient(cred, "ap-beijing", clientProfile)
+
+        req = models.TableOCRRequest()
+        params = '{"ImageUrl":"%s"}' % url
+
+        req.from_json_string(params)
+
+        resp = client.TableOCR(req)
+        r = json.loads(resp.to_json_string())
+        texts = r["TextDetections"]
+        row = 0
+        line = 0
+        for text in texts:
+            row = max(row, text["RowTl"])
+            line = max(line, text["ColTl"])
+        dict = {}
+        line = line + 1
+        row = row + 1
+        sheet = [[""] * line for i in range(row)]
+        for text in texts:
+            sheet[text["RowTl"]][text["ColTl"]] = text["Text"].strip()
+        dict["status"] = "succeed"
+        dict["row"] = row
+        dict["line"] = line
+        dict["sheet"] = sheet
+        return dict
+
+    except TencentCloudSDKException as err:
+        dict = {}
+        dict["status"] = "failed"
+        dict["err"] = err
+        return dict
+
+
+def tencent_ocr_handler(file):
+    url = upload_bucket(file)
+    form_info = tencent_ocr_api(url)
+    if form_info["status"] == "succeed":
+        return True, "\n".join(",".join(form_info["sheet"][i]) for i in range(form_info["row"]))
+    else:
+        return False, form_info["err"]
